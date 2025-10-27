e2b679518ca32de402024f3b6a7dbd0380210c72
rajdev2007 <raj.vibes2007@gmail.com>
2025-10-27 23:52:54 +0530

    added routes

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,35 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://rajvibes2007_db_user:QQcc%40123@cluster0.s4xc7k9.mongodb.net/paytm");
 
 const userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    firstname: String,
-    lastname: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    }
+},
+    {password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+},
+    {firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30
+    }
+},
+    {lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30
+    }
 })
 
 const User = mongoose.model("user", userSchema);
