4dd9fb34a9c07e8849ed42dd78a6006fa62195f6
rajdev2007 <raj.vibes2007@gmail.com>
2025-10-30 14:43:56 +0530

    fixed syntax errors

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://rajvibes2007_db_user:QQcc%40123@cluster0.s4xc7k9.mongodb.net/paytm");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -11,31 +11,42 @@ const userSchema = mongoose.Schema({
         lowercase: true,
         minLength: 3,
         maxLength: 30
-    }
-},
-    {password: {
+    },
+    password: {
         type: String,
         required: true,
         minLength: 6
-    }
-},
-    {firstname: {
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30
+    },
+    lastname: {
         type: String,
         required: true,
         trim: true,
         maxLength: 30
     }
-},
-    {lastname: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 30
+});
+
+const accountSchema = new mongoose.Schema({
+    UserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
     }
 })
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-    User
+    User,
+    Account
 }
