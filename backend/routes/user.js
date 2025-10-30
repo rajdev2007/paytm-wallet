16e3d1d6bfd4444fb5e4c8943de4ed6e0b7ca6a5
rajdev2007 <raj.vibes2007@gmail.com>
2025-10-30 14:11:12 +0530

    users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -92,4 +92,29 @@ router.post("/update", authMiddleware, async(req, res)=>{
     })
 })
 
+router.get("/bulk", async(req,res)=>{
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstname: {
+                "$regex": filter
+            }
+        },{
+            lastname: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map((user) => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 module.exports = router;
